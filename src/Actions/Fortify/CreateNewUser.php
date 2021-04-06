f91dfb26a97d9bb503c9b9f5f45da8e01cdda01e
DaltonMcCleery <daltonmccleery@gmail.com>
2021-04-05 19:25:41 -0500

    Adds: publishable Actions

---
+++ src/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Truefrontier\TeamInvites\Actions\Fortify;
+
+use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Jetstream\Events\TeamMemberAdded;
+use Laravel\Jetstream\Events\AddingTeamMember;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Truefrontier\TeamInvites\Models\Invitation;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Create a newly registered user.
+     *
+     * @param  array  $input
+     * @return mixed
+     */
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+            'invite' => ['sometimes', 'nullable'],
+        ])->validate();
+
+        return DB::transaction(function () use ($input) {
+            return tap(
+	            app(config('team_invites.user_modal'))::create([
+                    'name' => $input['name'],
+                    'email' => $input['email'],
+                    'password' => Hash::make($input['password']),
+                ]),
+                function ($user) use ($input) {
+                    if (isset($input['invite'])) {
+                        if ($invitation = Invitation::where('code', $input['invite'])->first()) {
+                            if ($team = $invitation->team) {
+                                AddingTeamMember::dispatch($team, $user);
+
+                                $team->users()->attach($user, ['role' => $invitation->role]);
+
+                                $user->current_team_id = $team->id;
+                                $user->save();
+
+                                TeamMemberAdded::dispatch($team, $user);
+
+                                $invitation->delete();
+
+                                // Force immediate login
+                                Auth::login($user);
+                            }
+                        }
+                    } else {
+                        $this->createTeam($user);
+                    }
+                },
+            );
+        });
+    }
+
+    /**
+     * Create a personal team for the user.
+     *
+     * @param  $user
+     * @return void
+     */
+    protected function createTeam($user)
+    {
+        $user->ownedTeams()->save(
+	        app(config('team_invites.team_modal'))::forceCreate([
+                'user_id' => $user->id,
+                'name' => explode(' ', $user->name, 2)[0] . "'s Team",
+                'personal_team' => true,
+            ]),
+        );
+    }
+}
---
+++ src/Actions/Jetstream/InviteTeamMember.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Truefrontier\TeamInvites\Actions\Jetstream;
+
+use Illuminate\Validation\Rule;
+use Laravel\Jetstream\Jetstream;
+use Laravel\Jetstream\Rules\Role;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Truefrontier\TeamInvites\Models\Invitation;
+use Laravel\Jetstream\Events\InvitingTeamMember;
+use Laravel\Jetstream\Contracts\InvitesTeamMembers;
+
+class InviteTeamMember implements InvitesTeamMembers
+{
+	/**
+	 * Invite a new team member to the given team.
+	 *
+	 * @param  mixed  $user
+	 * @param  mixed  $team
+	 * @param  string  $email
+	 * @param  string|null  $role
+	 * @return void
+	 */
+	public function invite($user, $team, string $email, string $role = null)
+	{
+		Gate::forUser($user)->authorize('addTeamMember', $team);
+
+		$this->validate($team, $email, $role);
+
+		InvitingTeamMember::dispatch($team, $email, $role);
+
+		$invitation = Invitation::create([
+			'user_id' => $user->id,
+			'team_id' => $team->id,
+			'role' => $role,
+			'email' => $email,
+		]);
+
+		Mail::to($email)->send(new \Truefrontier\TeamInvites\Mail\InviteTeamMember($invitation));
+	}
+
+	/**
+	 * Validate the invite member operation.
+	 *
+	 * @param  mixed  $team
+	 * @param  string  $email
+	 * @param  string|null  $role
+	 * @return void
+	 */
+	protected function validate($team, string $email, ?string $role)
+	{
+		Validator::make(
+			[
+				'email' => $email,
+				'role' => $role,
+			],
+			$this->rules($team),
+			[
+				'email.unique' => __('This user has already been invited to the team.'),
+			],
+		)
+			->after($this->ensureUserIsNotAlreadyOnTeam($team, $email))
+			->validateWithBag('addTeamMember');
+	}
+
+	/**
+	 * Get the validation rules for inviting a team member.
+	 *
+	 * @param  mixed  $team
+	 * @return array
+	 */
+	protected function rules($team)
+	{
+		return array_filter([
+			'email' => [
+				'required',
+				'email',
+				Rule::unique('team_invitations')->where(function ($query) use ($team) {
+					$query->where('team_id', $team->id);
+				}),
+			],
+			'role' => Jetstream::hasRoles() ? ['required', 'string', new Role()] : null,
+		]);
+	}
+
+	/**
+	 * Ensure that the user is not already on the team.
+	 *
+	 * @param  mixed  $team
+	 * @param  string  $email
+	 * @return \Closure
+	 */
+	protected function ensureUserIsNotAlreadyOnTeam($team, string $email)
+	{
+		return function ($validator) use ($team, $email) {
+			$validator
+				->errors()
+				->addIf(
+					$team->hasUserWithEmail($email),
+					'email',
+					__('This user already belongs to the team.'),
+				);
+		};
+	}
+}
