5fac7618fa35c0c5a68ff274089e8649447c7a90
DaltonMcCleery <daltonmccleery@gmail.com>
2021-04-05 20:08:24 -0500

    Adds: custom Vue form for Jetstream

--- src/TeamInvitesServiceProvider.php
+++ src/TeamInvitesServiceProvider.php
@@ -89,6 +89,11 @@ class TeamInvitesServiceProvider extends ServiceProvider
 	    $this->publishes([
 		    __DIR__.'/resources/views' => resource_path('views/vendor/truefrontier/team_invites'),
 	    ], 'views');
+
+	    $this->publishes([
+		    __DIR__.'/resources/js/Components/BannerMessage.vue' => resource_path('js/Components'),
+		    __DIR__.'/resources/js/Pages/Auth/Register.vue' => resource_path('js/Pages/Auth'),
+	    ], 'vue');
     }
 
     /**
---
+++ src/resources/js/Components/BannerMessage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div v-if="message" class="alert alert-info">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
