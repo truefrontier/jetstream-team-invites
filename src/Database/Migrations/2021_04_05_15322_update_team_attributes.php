2ac8175bb002893a81eb2070d3b4c149d3584bcd
DaltonMcCleery <daltonmccleery@gmail.com>
2021-04-05 19:44:06 -0500

    Adds: Team field migration

---
+++ src/Database/Migrations/2021_04_05_15322_update_team_attributes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTeamAttributes extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('teams', function (Blueprint $table) {
+			$table->string('logo')->nullable();
+			$table->string('subdomain')->nullable()->unique();
+			$table->json('config')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('teams', function (Blueprint $table) {
+			$table->dropColumn(['logo', 'subdomain', 'config']);
+		});
+	}
+}
