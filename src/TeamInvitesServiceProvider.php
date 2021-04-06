7c6a580fa4d7313063d66616f161455b6e7eef19
DaltonMcCleery <daltonmccleery@gmail.com>
2021-04-06 16:20:30 -0500

    Fix: publish email template

--- src/TeamInvitesServiceProvider.php
+++ src/TeamInvitesServiceProvider.php
@@ -87,7 +87,7 @@ class TeamInvitesServiceProvider extends ServiceProvider
 	    ], 'config');
 
 	    $this->publishes([
-		    __DIR__.'/resources/views' => resource_path('views/vendor/truefrontier/team_invites'),
+		    __DIR__.'/resources/emails' => resource_path('views/vendor/truefrontier/team_invites'),
 	    ], 'views');
 
 	    $this->publishes([
