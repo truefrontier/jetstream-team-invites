772010cb220a3c55187f17781776cafb62f2494c
Kevin Kirchner <kevnk@hey.com>
2021-09-07 02:13:45 -0500

    Fix markdown issue

--- src/resources/emails/teams/invite.blade.php
+++ src/resources/emails/teams/invite.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
-    {{ $invite->user->name }} wants you to join their team: {{ $invite->team->name }}
+{{ $invite->user->name }} wants you to join their team: {{ $invite->team->name }}
 
-    @component('mail::button', ['url' => route('register', ['email' => $invite->email, 'invite' => $invite->code])])
-        Create Your Account
-    @endcomponent
+@component('mail::button', ['url' => route('register', ['email' => $invite->email, 'invite' => $invite->code])])
+Create Your Account
+@endcomponent
 
-    Looking forward to having you on the team!
+Looking forward to having you on the team!
 @endcomponent
