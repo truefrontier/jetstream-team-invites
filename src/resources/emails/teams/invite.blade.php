f7243830c5968f247bea7726c9b4dc9c2be7d4f6
Kevin Kirchner <kevnk@hey.com>
2021-09-25 21:11:38 -0500

    Give option for some users to login

--- src/resources/emails/teams/invite.blade.php
+++ src/resources/emails/teams/invite.blade.php
@@ -5,5 +5,13 @@
 Create Your Account
 @endcomponent
 
+<br><br>
+
+Already have a login?
+
+@component('mail::button', ['url' => route('team-invitations.accept', ['invitation' => $invite])])
+Login To Join
+@endcomponent
+
 Looking forward to having you on the team!
 @endcomponent
